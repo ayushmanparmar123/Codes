1a7d5828c206c7fc0e5586828b1c88cab5fc68d5
ayushmanparmar123 <sapnashukla.99@gmail.com>
2025-05-09 15:59:52 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+# Prompt user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display results
+print(f"Addition:= {addition}")
+print(f"Subtraction:{subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division:{division}")
